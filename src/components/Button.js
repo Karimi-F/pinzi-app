206be332bb953ed530f33d6558dd265a4659aae9
Karimi-F <fridah37karimi@gmail.com>
2025-05-24 01:06:05 +0300

    Created a button component

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+'use client';
+
+export default function Button({ children, onClick, type = "button", className = "" }) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`px-4 py-2 rounded-lg bg-red-600 text-white hover:bg-red-700 transition ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
