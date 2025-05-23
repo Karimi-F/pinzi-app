32af76b1de165f159978fd29feb80210215c9c7e
Karimi-F <fridah37karimi@gmail.com>
2025-05-23 17:52:39 +0300

    Creates the login button

---
+++ src/components/AuthButton.js
@@ -0,0 +1,16 @@
+// src/components/AuthButton.js
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function AuthButton() {
+  const { data: session } = useSession();
+
+  return session ? (
+    <button onClick={() => signOut()} className="p-2 bg-red-500 text-white rounded">
+      Sign out
+    </button>
+  ) : (
+    <button onClick={() => signIn("google")} className="p-2 bg-blue-500 text-white rounded">
+      Sign in with Google
+    </button>
+  );
+}
