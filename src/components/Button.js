fc8c45e6b192c0ba853b65631a41d820985f412e
Karimi-F <fridah37karimi@gmail.com>
2025-05-25 12:51:15 +0300

    Modifies the Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,11 @@
-'use client';
+"use client";
 
-export default function Button({ children, onClick, type = "button", className = "" }) {
+export default function Button({
+  children,
+  onClick,
+  type = "button",
+  className = "",
+}) {
   return (
     <button
       type={type}
