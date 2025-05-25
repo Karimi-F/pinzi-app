fdd9d88e96de4ba043f00f186ef6af7d794b964d
Karimi-F <fridah37karimi@gmail.com>
2025-05-25 12:50:19 +0300

    Modifies the about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,45 +1,58 @@
-'use client';
+"use client";
 
 import Button from "@/components/Button";
-import { ChevronRight, BookOpen, BarChart, User, CircleUserRound } from "lucide-react"
+import {
+  ChevronRight,
+  BookOpen,
+  BarChart,
+  User,
+  CircleUserRound,
+} from "lucide-react";
 
 export default function About() {
-    return(
-        <div className="bg-white text-black h-screen"> 
-            <h1 className="text-center text-3xl font-bold p-8"> Why Use Pinzi Flashcards?</h1>
-            <div className="flex items-center">
-            
-            <div className="flex space-x-6 p-3">
-                <div className="rounded-md p-6 border border-gray-300">
-                    <div className="rounded-full bg-rose-300 w-12 h-12 flex items-center justify-center mb-4">
-                    <BookOpen className="h-6 w-6 text-red-600 bg-rose 500" />
-                    </div>
-                    <h2 className="font-bold text-2xl">Learn Faster</h2>
-                    <p>Interactive flashcards with Hanzi, Pinyin, and meaning all in one place</p>
-                </div>
-                <div className="rounded-md p-6 border border-gray-300">
-                    <div className="rounded-full bg-rose-300 w-12 h-12 flex items-center justify-center mb-4">
-                    <BarChart className="h-6 w-6 text-red-600 bg-rose 500" />
-                    </div>
-                    <h2 className="font-bold text-2xl">Track Progress</h2>
-                    <p>Save your progress and review cards you find challenging.</p>
-                </div>
-                <div className="rounded-md p-6 border border-gray-300">
-                    <div className="rounded-full bg-rose-300 w-12 h-12 flex items-center justify-center mb-4">
-                    <User className="h-6 w-6 text-red-600 bg-rose 500" />
-                    </div>
-                    
-                    <h2 className="font-bold text-2xl">Personalized Experience</h2>
-                    <p>Choose difficulty levels and customize your learning journey</p>
-                </div>
+  return (
+    <div className="bg-white text-black h-screen">
+      <h1 className="text-center text-3xl font-bold p-8">
+        {" "}
+        Why Use Pinzi Flashcards?
+      </h1>
+      <div className="flex items-center">
+        <div className="flex space-x-6 p-3">
+          <div className="rounded-md p-6 border border-gray-300">
+            <div className="rounded-full bg-rose-300 w-12 h-12 flex items-center justify-center mb-4">
+              <BookOpen className="h-6 w-6 text-red-600 bg-rose 500" />
             </div>
+            <h2 className="font-bold text-2xl">Learn Faster</h2>
+            <p>
+              Interactive flashcards with Hanzi, Pinyin, and meaning all in one
+              place
+            </p>
+          </div>
+          <div className="rounded-md p-6 border border-gray-300">
+            <div className="rounded-full bg-rose-300 w-12 h-12 flex items-center justify-center mb-4">
+              <BarChart className="h-6 w-6 text-red-600 bg-rose 500" />
+            </div>
+            <h2 className="font-bold text-2xl">Track Progress</h2>
+            <p>Save your progress and review cards you find challenging.</p>
+          </div>
+          <div className="rounded-md p-6 border border-gray-300">
+            <div className="rounded-full bg-rose-300 w-12 h-12 flex items-center justify-center mb-4">
+              <User className="h-6 w-6 text-red-600 bg-rose 500" />
+            </div>
+
+            <h2 className="font-bold text-2xl">Personalized Experience</h2>
+            <p>Choose difficulty levels and customize your learning journey</p>
+          </div>
         </div>
-        <div className="flex flex-col items-center w-full space-y-6 bg-slate-50 m-4 p-4">
-            <h1 className="text-3xl font-bold">Ready to start learning?</h1>
-            <p className="text-2xl">Join thousands of learners mastering Mandarin with our flashcard system.</p>
-            <Button className="">Get Started Now</Button>
-        </div>
-        </div>
-        
-    )
+      </div>
+      <div className="flex flex-col items-center w-full space-y-6 bg-slate-50 m-4 p-4">
+        <h1 className="text-3xl font-bold">Ready to start learning?</h1>
+        <p className="text-2xl">
+          Join thousands of learners mastering Mandarin with our flashcard
+          system.
+        </p>
+        <Button className="">Get Started Now</Button>
+      </div>
+    </div>
+  );
 }
