6423311aad69de12fe4b3d41706c31ff1ca0a144
Karimi-F <fridah37karimi@gmail.com>
2025-06-05 13:32:30 +0300

    Adds the map to the contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -91,15 +91,12 @@ export default function Contact() {
 
       <div>
         <div className="w-full h-64 md:h-full rounded-lg overflow-hidden shadow-lg">
-          <iframe
-            title="Google Map"
-            width="100%"
-            height="100%"
-            frameBorder="0"
-            style={{ border: 0 }}
-            src="https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=Your+Business+Address"
-            allowFullScreen
-          ></iframe>
+          <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1328.1129508520942!2d37.10873865326021!3d-1.7775699632940334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182fede2be180b0f%3A0x2e461af87d59b763!2sKonza!5e0!3m2!1sen!2ske!4v1749119053059!5m2!1sen!2ske" 
+          style={{ width: "100%", height: "400px", border: 0 }}
+          allowFullScreen="" 
+          loading="lazy" 
+          referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
 
@@ -108,7 +105,7 @@ export default function Contact() {
           <h5 className="text-2xl">FAQ</h5>
           <h1 className="font-bold text-4xl">Frequently asked questions</h1>
         </div>
-        
+
         <div>
           <ul>
             <li>
