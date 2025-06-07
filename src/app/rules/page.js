92f6cebd08c2ae31a4520de3a7770207ab9ed06b
Karimi-F <fridah37karimi@gmail.com>
2025-06-07 12:07:29 +0300

    Adds the Nav Bar to the rules page

--- src/app/rules/page.js
+++ src/app/rules/page.js
@@ -1,7 +1,11 @@
 'use client';
 
+import Navbar from "@/components/NavBar";
+
 export default function Rules(){
     return (
-        <div></div>
+        <div className="text-black bg-slate-50 min-h-screen">
+            <Navbar />
+        </div>
     )
 }
