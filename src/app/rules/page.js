ab5eda803f87da362d3f02a203d75830a50724f7
Karimi-F <fridah37karimi@gmail.com>
2025-06-07 12:08:02 +0300

    Adds the footer to the rules page

--- src/app/rules/page.js
+++ src/app/rules/page.js
@@ -1,11 +1,13 @@
 'use client';
 
 import Navbar from "@/components/NavBar";
+import Footer from "../footer/page";
 
 export default function Rules(){
     return (
         <div className="text-black bg-slate-50 min-h-screen">
             <Navbar />
+            <Footer />
         </div>
     )
 }
